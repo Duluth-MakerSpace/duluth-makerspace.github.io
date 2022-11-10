599144d80b170c0527542fb71c9926c59503a880
Tim Carlson <tim.carlson@gmail.com>
2022-11-09 21:04:40 -0600

    update basepath

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://duluth-makerspace.github.io',
-    base: '/',
+    site: 'https://duluth-makerspace.github.io'
 });
